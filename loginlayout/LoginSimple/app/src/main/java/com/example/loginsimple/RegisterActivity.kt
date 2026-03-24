be1f23604f99ff23b6a0c830b3e9ba8f59109a1e
Carlos <zamarra107@gmail.com>
2026-03-24 20:58:36 +0100

    LoginLayout

---
+++ loginlayout/LoginSimple/app/src/main/java/com/example/loginsimple/RegisterActivity.kt
@@ -0,0 +1,28 @@
+package com.example.loginsimple
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register_main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+        btnRegister.setOnClickListener {
+
+            finish()
+        }
+    }
+}
